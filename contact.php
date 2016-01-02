51a757b34c507c05d096b3657356c9743500dcc8
Christopher Collins <chris@spring215.com>
2016-01-02 15:15:22 -0500

    layout updates

--- contact.php
+++ contact.php
@@ -6,9 +6,10 @@ $companyLastName = $_POST['companyLastName'];
 $companyEmail = $_POST['companyEmail'];
 $companyPosition = $_POST['companyPosition'];
 $companyPhone = $_POST['companyPhone'];
+$companyMsg = $_POST['companyMsg'];
 $to = 'sales@jettisystems.com';
 $subject = 'Jetti Rewards Information Request';
-$message = 'COMPANY: ' . $companyName ."\n" . 'PHONE: ' . $companyPhone . "\n" . 'NAME: ' . $companyFirstName . ' ' . $companyLastName . "\n" . 'POSITION: ' . $companyPosition . "\n" . 'EMAIL: ' . $companyEmail . "\n" . 'PHONE: ' . $companyPhone . "\n" . 'WEB: ' . $companyUrl;
+$message = 'COMPANY: ' . $companyName ."\n" . 'PHONE: ' . $companyPhone . "\n" . 'NAME: ' . $companyFirstName . ' ' . $companyLastName . "\n" . 'POSITION: ' . $companyPosition . "\n" . 'EMAIL: ' . $companyEmail . "\n" . 'PHONE: ' . $companyPhone . "\n" . 'WEB: ' . $companyUrl . "\n" . 'WEB: ' . $companyMsg;
 mail ($to, $subject, $message, 'From: ' . $companyName);
 header("Location: http://www.jettisystems.com/thank-you.html");
 ?>
