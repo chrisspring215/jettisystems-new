1c504c16ee143f6750f67538ac12fcfb843d3c6f
Christopher Collins <chris@spring215.com>
2016-01-05 15:46:55 -0500

    added screen shot to home page, added links on home page, fixed active sub nav button color

--- js/build.js
+++ js/build.js
@@ -22,21 +22,21 @@ function buildGlobalNav() {
 	document.getElementById('globalNavsecond2-1Dropdown').innerHTML = 'CUSTOMER EMAILS';
 	document.getElementById('globalNavsecond2-1Dropdown').setAttribute('href', 'restaurant-rewards-benefits-customer-emails.html');
 	var benefitsSubNav1 = document.getElementById('benefitsSubNav1');
-	if (benefitsSubNav1 != null) document.getElementById('benefitsSubNav1').innerHTML = '<button class="button expanded active">1. GATHER EMAILS</button>';
+	if (benefitsSubNav1 != null) document.getElementById('benefitsSubNav1').innerHTML = '<button class="button expanded">1. GATHER EMAILS</button>';
 	if (benefitsSubNav1 != null) document.getElementById('benefitsSubNav1').setAttribute('href', 'restaurant-rewards-benefits-customer-emails.html');
 
 	// Sets Second Link Button 2.2 Text
 	document.getElementById('globalNavsecond2-2Dropdown').innerHTML = 'DRIVE UPSELLING';
 	document.getElementById('globalNavsecond2-2Dropdown').setAttribute('href', 'restaurant-rewards-benefits-customer-upselling.html');
 	var benefitsSubNav2 = document.getElementById('benefitsSubNav2');
-	if (benefitsSubNav2 != null) document.getElementById('benefitsSubNav2').innerHTML = '<button class="button expanded active">2. DRIVE UPSELLING</button>';
+	if (benefitsSubNav2 != null) document.getElementById('benefitsSubNav2').innerHTML = '<button class="button expanded">2. DRIVE UPSELLING</button>';
 	if (benefitsSubNav2 != null) document.getElementById('benefitsSubNav2').setAttribute('href', 'restaurant-rewards-benefits-customer-upselling.html');
 
 	// Sets Second Link Button 2.2 Text
 	document.getElementById('globalNavsecond2-3Dropdown').innerHTML = 'BUILD LOYALTY';
 	document.getElementById('globalNavsecond2-3Dropdown').setAttribute('href', 'restaurant-rewards-benefits-build-brand-loyalty.html');
 	var benefitsSubNav3 = document.getElementById('benefitsSubNav3');
-	if (benefitsSubNav3 != null) document.getElementById('benefitsSubNav3').innerHTML = '<button class="button expanded active">3. BUILD LOYALTY</button>';
+	if (benefitsSubNav3 != null) document.getElementById('benefitsSubNav3').innerHTML = '<button class="button expanded">3. BUILD LOYALTY</button>';
 	if (benefitsSubNav3 != null) document.getElementById('benefitsSubNav3').setAttribute('href', 'restaurant-rewards-benefits-build-brand-loyalty.html');
 
 
